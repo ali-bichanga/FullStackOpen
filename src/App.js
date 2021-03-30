b9570345ad8047eaf46ddf8d6185739258b4f79a
Alice <bichangaalice@gmail.com>
2021-03-30 10:40:20 +0300

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
+const Hello = () => {
   return(
-  <div>
-    <p>Hello World, it is {now.toString()}</p>
-    <p>
-      {a} plus {b} is {a + b}
-    </p>
-  </div>
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return(
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
   )
 }
 
